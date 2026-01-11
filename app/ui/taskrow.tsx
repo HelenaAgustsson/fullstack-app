bea7e2558b8c9c1cd441f9ffb3bcae09448d6e9f
Helena Agustsson <agustssonhelena@gmail.com>
2026-01-11 11:06:28 +0000

    quick fix rows

--- app/ui/taskrow.tsx
+++ app/ui/taskrow.tsx
@@ -19,9 +19,8 @@ export default function TaskRow({ task, people }: TaskRowProps) {
     return (
         <tr key={task.id} className="w-full border-b-5 border-solid border-gray-50 py-3 text-sm last-of-type:border-none">
             <td className="whitespace-nowrap py-3 pl-6 pr-3">{task.description}</td>
+            <td className="whitespace-nowrap py-3 pl-6 pr-3">{findPerson(people, task)}</td>
             <td className="whitespace-nowrap py-3 pl-6 pr-3">{task.done ? 'completed' : 'to do'}</td>
-            <td className="whitespace-nowrap py-3 pl-6 pr-3">{findPerson(people, task)}
-            </td>
         </tr>
     )
 }
